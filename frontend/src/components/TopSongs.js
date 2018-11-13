196ec8435a31c220ced5ce77ac477732d557d43b
John Nguyen <johntnguyen9@gmail.com>
2018-11-13 08:46:33 -0600

    (feat) add user's top tracks for 3 time periods and change styling of login page
    User's top tracks now are from short_term (last week), medium_term (6 months), long_term (years). Change styling of main page to have 2 distinct sections.

--- frontend/src/components/TopSongs.js
+++ frontend/src/components/TopSongs.js
@@ -6,56 +6,83 @@ class TopSongs extends Component{
 
         this.state = {
             tokens: this.props.token,
-            topTracks: []
+            topTracks: [],
+            timeRange: ['short_term','medium_term', 'long_term']
         }
 
     }
+
     getTracks = ()=>{
         // GET https://api.spotify.com/v1/me/top/tracks
         console.log(this.state.params)
+
+        let tracks = []
+        for(let timeSpan of this.state.timeRange){
+            tracks.push(
+            axios.get(`https://api.spotify.com/v1/me/top/tracks?time_range=${timeSpan}&limit=10&offset=5`, {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + this.state.tokens.access_token
+            }
+            })
+            .then((res) => {
+            console.log(res.data)
+            return res
+            })
+            .then((res) => {
+                // tracks.push(res.data.items)
+                console.log(tracks)
+                return res.data.items
+            })
+            .catch(function (res) {
+            console.log(res)
+            })
+            ) 
+        }
+        
+        Promise.all(tracks).then((values) => {
+            console.log(tracks)
+            this.setState({ topTracks: values }) }
+        )
+    }
     
-        axios.get("https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=10&offset=5", {
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + this.state.tokens.access_token
-          }
-        })
-        .then((res) => {
-          console.log(res.data)
-          return res.data
-        })
-        .then((res) => {
-          this.setState({topTracks:res.items})
-          
-        })
-        .catch(function (res) {
-          console.log(res)
-        }) 
-      }
+    
+      
 
     songCard = () => {
-        console.log("ahhh"+this.state.topTracks)
-        return this.state.topTracks.map( track => {
-        return(
-        <div class="card" key={track.uri}>
-            <div class="row">
-                <div class="col">
-                    <img src={track.album.images[3].url} alt="..." class="float-left"></img>
+        console.log("ahhh"+this.state.topTracks.length)
+        let result = []
+        for(let i = 0; i < this.state.topTracks.length; i++){
+            console.log(i+"asdsas")
+            result.push(
+                <div card="card">
+                 <h5 class="card-title">{this.state.timeRange[i]}</h5>
                 </div>
-                <div class="col">
-                    <div class="card-body">
-                    <h5 class="card-title">{track.name}</h5>
-                    <p class="card-text">{track.album.name}</p>
+            )
+            result.push(this.state.topTracks[i].map( track => {
+            return(
+            <div class="card" key={track.uri}>
+                <div class="row">
+                    <div class="col">
+                        <img src={track.album.images[1].url} alt="..." class="float-left"></img>
+                    </div>
+                    <div class="col">
+                        <div class="card-body">
+                        <h5 class="card-title">{track.name}</h5>
+                        <p class="card-text">{track.album.name}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            
+                
 
-         </div>
+            </div>
+            )
+            }
+            )
         )
         }
-        )
+        return result
     }
 
     
