72076897451c5c0f2477b2257e0f8c32c834453d
John Nguyen <johntnguyen9@gmail.com>
2018-11-19 20:28:06 -0600

    (styles:) reconfigure build to compile scss

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ node ./server/server.js
 
 npm install in frontend
 npm start
+
+## css
+This app is ran with a custom bootstrap theme edited in src/styles/scss/index.html
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import './bootstrap/css/bootstrap.css';
+import './styles/css/index.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
